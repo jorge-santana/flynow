ebe7014cbd560c66386d4f10db74b8e5c4fb98fa
Jorge Sant Ana <jorge@argus-academy.com>
2026-01-05 12:43:23 -0300

    chore: adiciona vpc e comenta versionamento tfstate*

---
+++ terraform/vpc.tf
@@ -0,0 +1,9 @@
+resource "aws_vpc" "vpc" {
+  cidr_block = "10.0.0.0/16"
+  region = "us-east-1"
+  enable_dns_support = true
+  enable_dns_hostnames = true
+  tags = {
+    Name = "tf_flynow_vpc"
+  }
+}
